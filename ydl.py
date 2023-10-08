2fcde08afbf3da633c9fffab4230259cd96528c3
user <mac@MacBook-Air-som-tillhor-Etiosa.local>
2023-10-08 21:18:03 +0200

    Initial commit

---
+++ ydl.py
@@ -0,0 +1,34 @@
+from pytube import YouTube
+from moviepy.editor import *
+
+
+url = 'https://www.youtube.com/watch?v=-T2TIJF9Xyk'
+
+yt = YouTube(url)
+
+#Get the audio stream
+audio_streaam = yt.streams.filter(only_audio=True, file_extension='webm').first()
+
+#Print video title
+print("Title: ", yt.title)
+
+#Download audio stream
+download_path = audio_streaam.download(output_path='/Users/mac/Desktop/Musica')
+
+# Convert downloaded audio to mp3
+
+audio_clip = AudioFileClip(download_path)
+mp3_path = download_path.replace(".webm", ".mp3")
+audio_clip.write_audiofile(mp3_path, codec='mp3')
+
+#Remove origial .webm file for storage purposes after conversion
+import os
+os.remove(download_path)
+
+print(f"Audio saved as ${mp3_path}")
+
+
+
+
+
+
